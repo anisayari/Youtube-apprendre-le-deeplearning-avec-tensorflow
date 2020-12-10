96eb2a3ee606987842ed979a0ae60058e1c8ae43
anisayari <anis.ayari.perso@gmail.com>
2020-12-10 12:56:49 +0100

    add pikachu 99 images

---
+++ #4 - CNN/download_images.py
@@ -0,0 +1,6 @@
+from google_images_download import google_images_download   #importing the library
+
+response = google_images_download.googleimagesdownload()   #class instantiation
+
+arguments = {"keywords":"Polar bears,baloons,Beaches","limit":20,"format":"jpg"}
+paths = response.download(arguments)
